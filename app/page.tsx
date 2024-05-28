4d8fc794a0951eed6e301104177e50bb02bebeaa
Omar-Tood <omarjibrilabdulkhadir@gmail.com>
2024-05-28 23:11:33 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex   items-center justify-between">
+     <h1>Welcome to Drizzle ORM </h1>
+    </main>
+  );
+
+}
