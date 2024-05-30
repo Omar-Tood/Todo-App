fd578c594887159bfdc13da28525cecfac1322ac
Omar-Tood <omarjibrilabdulkhadir@gmail.com>
2024-05-30 03:31:17 +0300

    fixed users

--- components/Todos.tsx
+++ components/Todos.tsx
@@ -19,10 +19,10 @@ const Todos: FC<Props> = ({ todos, user }) => {
   // Function to create a new todo item
   const createTodo = (text: string) => {
 
-    addUser()
-    // const id = (todoItems.at(-1)?.id || 0) + 1;
-    // addTodo(id, text,user?.id);
-    // setTodoItems((prev) => [...prev, { id: id, text, done: false, userId: user?.id }]);
+    // addUser()
+    const id = (todoItems.at(-1)?.id || 0) + 1;
+    addTodo(id, text,user?.id);
+    setTodoItems((prev) => [...prev, { id: id, text, done: false, userId: user?.id }]);
   };
 
   // Function to change the text of a todo item
